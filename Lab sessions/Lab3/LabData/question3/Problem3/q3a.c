e24bb678db55900fcb04f5b41d35d0f103dc1a7f
Rishikesh B <rishi.iithyd@gmail.com>
2025-09-02 02:32:59 +0530

    Been quite a time, just pushing to have stuff on sync

---
+++ Lab sessions/Lab3/LabData/question3/Problem3/q3a.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	FILE *file;
+	FILE *file2;
+	double col1[100];
+	double col2[100];
+	int n = 0;
+
+	file = fopen("ElectrostaticPotential.dat", "r");
+	file2 = fopen("Field.dat", "w");
+
+	if (file == NULL || file2 == NULL) {
+		printf("Error: Could not open file.\n");
+		return 1;
+	}
+
+	// Read until EOF or 100
+	while (n < 100 && fscanf(file, "%lf %lf", &col1[n], &col2[n]) == 2) {
+		n++;
+	}
+
+	if (n < 2) {
+		printf("Error: Not enough data.\n");
+		fclose(file);
+		fclose(file2);
+		return 1;
+	}
+
+	double derivative[100];
+
+	// forward difference for first point
+	derivative[0] = (col2[1] - col2[0]) / (col1[1] - col1[0]);
+
+	// central difference for middle points
+	for (int i = 1; i < n - 1; i++) {
+		derivative[i] = (col2[i+1] - col2[i-1]) / (col1[i+1] - col1[i-1]);
+	}
+
+	// backward difference for last point
+	derivative[n-1] = (col2[n-1] - col2[n-2]) / (col1[n-1] - col1[n-2]);
+
+	// write output
+	for (int i = 0; i < n; i++) {
+		fprintf(file2, "%lf %lf\n", col1[i], -derivative[i]);
+	}
+
+	fclose(file);
+	fclose(file2);
+
+	return 0;
+}
